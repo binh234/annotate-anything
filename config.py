71cf48177c3ead77825b12a8bdd148ffb7f993c0
Binh Le <binhnd234@gmail.com>
2023-06-04 16:56:59 +0700

    Update configurations

--- config.py
+++ config.py
@@ -1,4 +1,44 @@
 # Configurations
+tag2text_dict = {
+    "swin_14m": {
+        "checkpoint_url": "https://huggingface.co/spaces/xinyu1205/Tag2Text/resolve/main/tag2text_swin_14m.pth",
+        "checkpoint_file": "tag2text_swin_14m.pth",
+    }
+}
+
+sam_dict = {
+    "default": {
+        "checkpoint_url": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
+        "checkpoint_file": "sam_vit_h_4b8939.pth",
+    },
+    "vit_h": {
+        "checkpoint_url": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
+        "checkpoint_file": "sam_vit_h_4b8939.pth",
+    },
+    "vit_l": {
+        "checkpoint_url": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth",
+        "checkpoint_file": "sam_vit_l_0b3195.pth",
+    },
+    "vit_b": {
+        "checkpoint_url": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth",
+        "checkpoint_file": "sam_vit_b_01ec64.pth",
+    },
+}
+
+dino_dict = {
+    "swinb": {
+        "repo_id": "ShilongLiu/GroundingDINO",
+        "config_file": "GroundingDINO_SwinB.cfg.py",
+        "checkpoint_file": "groundingdino_swinb_cogcoor.pth",
+    },
+    "swint_ogc": {
+        "repo_id": "ShilongLiu/GroundingDINO",
+        "config_file": "GroundingDINO_SwinT_OGC.cfg.py",
+        "checkpoint_file": "groundingdino_swint_ogc.pth",
+    },
+}
+
+weight_dir = "weights"
 tag2text_checkpoint = "tag2text_swin_14m.pth"
 tag2text_url = "https://huggingface.co/spaces/xinyu1205/Tag2Text/resolve/main/tag2text_swin_14m.pth"
 sam_checkpoint = "sam_vit_h_4b8939.pth"
